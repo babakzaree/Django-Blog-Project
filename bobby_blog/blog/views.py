8bcbbcd281723387e1e0d10c66fa707f47205e46
Babakzare <babakzare2250@gmail.com>
2022-08-21 12:48:52 +0430

    Added Templates and modified settings.

--- bobby_blog/blog/views.py
+++ bobby_blog/blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+posts= [
+    {
+        'user':'Babak zare',
+        'title':'First post of my blog!',
+        'content':'this is the first post of my blog! I am so happy :)',
+        'date_posted':'8/20/2022'
+    },
+    {
+        'user':'Saeed Lashkari',
+        'title':'What a Walking dead!',
+        'content':'The walking dead show is amazing. I LOVE IT!',
+        'date_posted':'8/21/2022'
+    }
+]
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts ,
+        'title':'Home'
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html',{'title':'About'})
